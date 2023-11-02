b7a7fb3e7b82dcb923e2006760c876db27d0f3e8
AsyJAIZ <2007andrylavr@gmail.com>
2023-11-02 22:23:34 +0100

    Set dimens, refactor code

--- app/src/main/java/com/aj/roundrectoverlay/Hook.java
+++ app/src/main/java/com/aj/roundrectoverlay/Hook.java
@@ -17,16 +17,16 @@ public class Hook implements IXposedHookInitPackageResources, IXposedHookZygoteI
     String resources;
     @Override
     public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resParam) throws Throwable {
+        XModuleResources res = XModuleResources.createInstance(resources, null);
         if (resParam.packageName.equals(system)) {
             XResources.setSystemWideReplacement(system, "bool", "config_useRoundIcon", false);
             XResources.setSystemWideReplacement(system, "dimen", "config_bottomDialogCornerRadius",
-                    new XResources.DimensionReplacement(4.0f, TypedValue.COMPLEX_UNIT_DIP));
+                    res.fwd(R.dimen.bottomDialogCornerRadius));
             XResources.setSystemWideReplacement(system, "dimen", "config_dialogCornerRadius",
-                    new XResources.DimensionReplacement(2.0f, TypedValue.COMPLEX_UNIT_DIP));
-            XResources.setSystemWideReplacement(system, "string", "config_icon_mask",
+                    res.fwd(R.dimen.dialogCornerRadius));
+            XResources.setSystemWideReplacement(system, "string", "config_icon_mask",a
                     "M50,0L88,0 C94.4,0 100,5.4 100 12 L100,88 C100,94.6 94.6 100 88 100 L12,100 C5.4,100 0,94.6 0,88 L0 12 C0 5.4 5.4 0 12 0 L50,0 Z");
 
-            XModuleResources res = XModuleResources.createInstance(resources, null);
             XResources.setSystemWideReplacement(system, "drawable", "notification_icon_circle",
                     res.fwd(R.drawable.notification_icon_circle));
 
@@ -39,32 +39,31 @@ public class Hook implements IXposedHookInitPackageResources, IXposedHookZygoteI
 
         } else if (resParam.packageName.equals(ui)) {
             resParam.res.setReplacement(ui, "dimen", "global_actions_corner_radius",
-                    new XResources.DimensionReplacement(4.0f, TypedValue.COMPLEX_UNIT_DIP));
+                    res.fwd(R.dimen.bottomDialogCornerRadius));
             resParam.res.setReplacement(ui, "dimen", "rounded_slider_corner_radius",
-                    new XResources.DimensionReplacement(4.0f, TypedValue.COMPLEX_UNIT_DIP));
+                    res.fwd(R.dimen.bottomDialogCornerRadius));
             resParam.res.setReplacement(ui, "dimen", "rounded_slider_track_inset",
-                    new XResources.DimensionReplacement(2.0f, TypedValue.COMPLEX_UNIT_DIP));
+                    res.fwd(R.dimen.dialogCornerRadius));
             resParam.res.setReplacement(ui, "dimen", "rounded_slider_background_rounded_corner",
-                    new XResources.DimensionReplacement(10.0f, TypedValue.COMPLEX_UNIT_DIP));
-            resParam.res.setReplacement(ui, "dimen", "qs_security_footer_corner_radius",
-                    new XResources.DimensionReplacement(4.0f, TypedValue.COMPLEX_UNIT_DIP));
+                    res.fwd(R.dimen.rounded_slider_background_rounded_corner));
+            /*resParam.res.setReplacement(ui, "dimen", "qs_security_footer_corner_radius",
+                    new XResources.DimensionReplacement(4.0f, TypedValue.COMPLEX_UNIT_DIP));*/
             resParam.res.setReplacement(ui, "dimen", "qs_corner_radius",
-                    new XResources.DimensionReplacement(6.0f, TypedValue.COMPLEX_UNIT_DIP));
+                    res.fwd(R.dimen.qs_corner_radius));
             resParam.res.setReplacement(ui, "dimen", "qs_media_album_radius",
-                    new XResources.DimensionReplacement(4.0f, TypedValue.COMPLEX_UNIT_DIP));
+                    res.fwd(R.dimen.bottomDialogCornerRadius));
             resParam.res.setReplacement(ui, "dimen", "notification_corner_radius",
-                    new XResources.DimensionReplacement(4.0f, TypedValue.COMPLEX_UNIT_DIP));
+                    res.fwd(R.dimen.bottomDialogCornerRadius));
             resParam.res.setReplacement(ui, "dimen", "notification_corner_radius_small",
-                    new XResources.DimensionReplacement(2.0f, TypedValue.COMPLEX_UNIT_DIP));
+                    res.fwd(R.dimen.dialogCornerRadius));
             resParam.res.setReplacement(ui, "dimen", "notification_scrim_corner_radius",
                     new XResources.DimensionReplacement((32.0f/28.0f)*4.0f, TypedValue.COMPLEX_UNIT_DIP));
 
-            XModuleResources res = XModuleResources.createInstance(resources, null);
-            resParam.res.setReplacement(ui, "drawable", "qs_tile_background_shape",
+            /*resParam.res.setReplacement(ui, "drawable", "qs_tile_background_shape",
                     res.fwd(R.drawable.qs_tile_background_shape));
             resParam.res.setReplacement(ui, "drawable", "qs_media_outline_layout_bg",
                     res.fwd(R.drawable.qs_media_outline_layout_bg));
-            /*resParam.res.setReplacement(ui, "drawable", "qs_media_solid_button",
+            resParam.res.setReplacement(ui, "drawable", "qs_media_solid_button",
                     res.fwd(R.drawable.qs_media_solid_button));*/
             /*resParam.res.setReplacement(ui, "dimen", "qs_media_album_radius",
                     new XResources.DimensionReplacement(4.0f, TypedValue.COMPLEX_UNIT_DIP));*/
