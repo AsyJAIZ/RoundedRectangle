fe58c77fb17c0f525c0567c784d43da131fc7951
AsyJAIZ <2007andrylavr@gmail.com>
2023-11-02 22:38:33 +0100

    Fix Hook.java

--- app/src/main/java/com/aj/roundrectoverlay/Hook.java
+++ app/src/main/java/com/aj/roundrectoverlay/Hook.java
@@ -24,7 +24,7 @@ public class Hook implements IXposedHookInitPackageResources, IXposedHookZygoteI
                     res.fwd(R.dimen.bottomDialogCornerRadius));
             XResources.setSystemWideReplacement(system, "dimen", "config_dialogCornerRadius",
                     res.fwd(R.dimen.dialogCornerRadius));
-            XResources.setSystemWideReplacement(system, "string", "config_icon_mask",a
+            XResources.setSystemWideReplacement(system, "string", "config_icon_mask",
                     "M50,0L88,0 C94.4,0 100,5.4 100 12 L100,88 C100,94.6 94.6 100 88 100 L12,100 C5.4,100 0,94.6 0,88 L0 12 C0 5.4 5.4 0 12 0 L50,0 Z");
 
             XResources.setSystemWideReplacement(system, "drawable", "notification_icon_circle",
